e33e5fbdeca1e7b06f952fb5556acd9c73ee7b42
본ì권순혁 <sun970830@gmail.com>
2025-10-27 06:33:33 +0900

    feat:자동차이름은 쉼표를 기준으로 구분하며 이름은 5자이하만 가느능하다

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,7 +2,6 @@ package racingcar;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Java Version: " + System.getProperty("java.version"));
-        System.out.println("Java Home: " + System.getProperty("java.home"));
+
     }
 }
--- src/main/java/racingcar/domain/CarName.java
+++ src/main/java/racingcar/domain/CarName.java
@@ -1,5 +1,19 @@
 package racingcar.domain;
 
 public class CarName {
+    public static final int maxNameLength = 5;
+
+    private String value;
+
+    public CarName(String name) {
+        if (name.length() > maxNameLength) {
+            throw new IllegalArgumentException("차 이름을" + + maxNameLength + "넘길수없습니다" );
+        }
+        this.value = name;
+    }
+    public String value() {
+        return value;
+    }
 
 }
+
